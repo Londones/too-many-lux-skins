9584298b5f3c65408f6ea6146474db590ca08062
Bah Awa <bah.awaa@gmail.com>
2023-08-12 04:04:58 +0200

    tailwindcss start of search bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import React, { useEffect, useState } from "react";
 import logo from "./assets/logo.svg";
 import "./assets/App.css";
-import { SkinFetcherService } from "./services/skin-fetcher.service";
+import { ChampionFetcherService } from "./services/champion-fetcher.service";
 import { fetchVersion } from "./services/lastestversion-fetcher.service";
+import { AutoComplete } from "antd";
 
 function App() {
   const [champion, setChampion] = useState<string>("Aatrox");
   const [championData, setChampionData] = useState<any>(null);
-  const skinFetcher = new SkinFetcherService("fr_FR", "13.12.1");
+  const championFetcher = new ChampionFetcherService("fr_FR", "13.12.1");
+  const [championsArray, setChampionsArray] = useState<string[]>([]);
+  const [showDropdown, setShowDropdown] = useState(false); // add state variable to control dropdown visibility
 
   useEffect(() => {
     console.log(fetchVersion().then((res) => console.log(res)));
@@ -15,30 +18,42 @@ function App() {
 
   useEffect(() => {
     const fetchData = async () => {
-      const data = await skinFetcher.returnChampionObject(champion);
+      const data = await championFetcher.returnChampionObject(champion);
+      const championsArray = await championFetcher.fetchChampionsName();
       setChampionData(data);
+      setChampionsArray(championsArray);
+      console.log(championsArray);
       console.log(data);
     };
 
     fetchData();
   }, [champion]);
 
+  const handleFocus = () => {
+    setShowDropdown(false); // hide the dropdown when the input is focused
+  };
+
+  const handleSearch = (value: string) => {
+    if (value === "") {
+      setShowDropdown(false);
+    } else {
+      setShowDropdown(true);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AutoComplete
+        className="w-[50%]"
+        placeholder="Search a champion"
+        options={championsArray.map((champion) => ({
+          value: champion,
+        }))}
+        filterOption={true}
+        open={showDropdown} // use the state variable to control the dropdown visibility
+        onFocus={handleFocus} // hide the dropdown when the input is focused
+        onSearch={handleSearch}
+      />
     </div>
   );
 }
--- src/services/champion-fetcher.service.ts
+++ src/services/champion-fetcher.service.ts
@@ -1,10 +1,28 @@
-export class ChampionsNameFetcher {
+export class ChampionFetcherService {
   private language: string;
   private version: string;
 
   constructor(language: string, version: string) {
     this.language = language;
     this.version = version;
+  }
+
+  public async fetchChampion(championName: string): Promise<any> {
+    let json: Promise<any>;
+
+    const promise = await fetch(
+      `http://ddragon.leagueoflegends.com/cdn/${this.version}/data/${this.language}/champion/${championName}.json`
+    );
+
+    json = await promise.json();
+
+    return json;
+  }
+
+  public async returnChampionObject(champion: string): Promise<Object> {
+    return await this.fetchChampion(champion).then((data) => {
+      return data.data[champion];
+    });
   }
 
   public async fetchChampionsName(): Promise<string[]> {
