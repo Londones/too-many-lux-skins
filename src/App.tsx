221de2ead77931b2a080701757922964cc47ef3f
Londones <95936610+Londones@users.noreply.github.com>
2023-12-12 19:22:14 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
   const preferedLangage = localStorage.getItem("langage");
   const [langage, setLangage] = useState<string>(preferedLangage ?? "en_US");
   const [champion, setChampion] = useState<any>(null);
-  const [version, setVersion] = useState<string>("13.24");
+  const [version, setVersion] = useState<string>("13.24.1");
   const championFetcher = new ChampionFetcherService(version, langage);
   const [languages, setLanguages] = useState<string[]>([]);
 
